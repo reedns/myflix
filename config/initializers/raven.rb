82c0fb23544709a722f5c726c4b8256539c5fdcb
Reed Nelson-Saunders <reedns@hotmail.com>
2014-08-07 15:16:09 -0700

    added staging environment and raven initializer

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,5 +1,5 @@
 require 'raven'
 
 Raven.configure do |config|
-  config.dsn = ENV['SENTRY_DSN']
+  config.dsn = 'https://0d6a2656c156416582cff564b55b8c74:0f7b6c51d0b142cab6936ecfad6e82dd@app.getsentry.com/2858'
 end
